01ef5c0268dee7839d85e3c97afb5988d2b87e9b
erluxman <erluxman@gmail.com>
2026-05-17 15:42:23 +0545

    initial

---
+++ app/src/main/java/com/erluxman/focuslauncher/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.erluxman.focuslauncher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange80 = Color(0xFFFFB870)
+val OrangeGrey80 = Color(0xFFDFC4A2)
+val Amber80 = Color(0xFFEFD08C)
+
+val Orange40 = Color(0xFF8B5000)
+val OrangeGrey40 = Color(0xFF715B41)
+val Amber40 = Color(0xFF6D5E00)
+
+val WarmWhite = Color(0xFFFFFBf2)
+val WarmDark = Color(0xFF1C1B17)
